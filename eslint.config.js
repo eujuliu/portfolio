b9af894e16fd541ba473d6a569ed9cd292c68062
Julio Martins <juliomarttins5@gmail.com>
2025-02-27 00:45:41 -0300

    chore(lint): configured eslint + prettier
    now the projects have linting configuration for .astro, .ts, .tsx with
    a11y for acessibility

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import eslint from "@eslint/js";
+import ts from "typescript-eslint";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import astro from "eslint-plugin-astro";
+import a11y from "eslint-plugin-jsx-a11y";
+import prettier from "eslint-config-prettier";
+
+export default ts.config(
+	eslint.configs.recommended,
+	...astro.configs.recommended,
+	a11y.flatConfigs.strict,
+	{ ...react.configs.flat.recommended, files: ["src/**/*.{tsx,ts}"] },
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
+		},
+		ignores: ["!**/*.{tsx,ts,astro}"],
+	},
+	prettier,
+);
