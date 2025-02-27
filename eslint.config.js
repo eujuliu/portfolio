36f862617389688717350eefea520f0bc4f9adb2
Julio Martins <juliomarttins5@gmail.com>
2025-02-27 13:21:24 -0300

    chore(fix): solve `Parsing error: Unexpected token, expected`
    eslint error solved when tsparse was added to react plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,39 @@
-import eslint from "@eslint/js";
 import ts from "typescript-eslint";
 import globals from "globals";
+import eslint from "@eslint/js";
 import react from "eslint-plugin-react";
 import astro from "eslint-plugin-astro";
 import a11y from "eslint-plugin-jsx-a11y";
 import prettier from "eslint-config-prettier";
+import tsParser from "@typescript-eslint/parser";
 
 export default ts.config(
 	eslint.configs.recommended,
 	...astro.configs.recommended,
-	a11y.flatConfigs.strict,
-	{ ...react.configs.flat.recommended, files: ["src/**/*.{tsx,ts}"] },
 	{
+		...react.configs.flat.recommended,
+		files: ["src/**/*.{tsx,ts}"],
+		languageOptions: {
+			...react.configs.flat.recommended.languageOptions,
+			parser: tsParser,
+		},
+	},
+	a11y.flatConfigs.strict,
+	{
+		settings: {
+			react: {
+				version: "detect",
+			},
+		},
 		languageOptions: {
 			globals: {
 				...globals.browser,
 			},
 		},
+		ignores: ["!**/*.{tsx,ts,astro}"],
 		rules: {
 			"react/react-in-jsx-scope": "off",
 		},
-		ignores: ["!**/*.{tsx,ts,astro}"],
 	},
 	prettier
 );
